758aa4c8b969cd8554605ff5d441c7273abb5035
k-ccosta <k.accosta98@hotmail.com>
2025-12-19 11:40:29 -0400

    feat: captura submit e adiciona tarefas na lista

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-console.log("TaskFlow iniciado")
+const form = document.querySelector("#task-form");
+const taskInput = document.querySelector("#task-input");
+const taskList = document.querySelector("#task-list");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const taskValue = taskInput.value;
+
+    const taskItemTrimmed = taskValue.trim();
+
+    if (taskItemTrimmed === "") {
+        alert("Por favor, digite uma tarefa válida.");
+        return;
+    }
+
+    const taskItem = document.createElement("li")
+    taskItem.textContent = taskItemTrimmed;
+    taskList.append(taskItem);
+    taskInput.value = "";
+    taskInput.focus();
+});
